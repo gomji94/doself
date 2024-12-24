e45d23958269d87d9c25eb194683e2a93a56b086
steaming121 <rlaskdus66@gmail.com>
2024-12-24 17:17:59 +0900

    [doself-kny] 헤더 푸터 작업중(작업 미완) detail : 헤더 푸터 html, css 작업 진행완(아마도...?)/default에서 fragment 불러오기, 인덱스에 defalt 불러오기 작업 미완

---
+++ doself/src/main/java/doself/user/challenge/controller/ChallengeController.java
@@ -0,0 +1,16 @@
+package doself.user.challenge.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+@RequestMapping("/user")
+public class ChallengeController {
+	@GetMapping(value={"","/"})
+	public String defaultTest() {
+		return "user/index";
+	}
+	
+}
