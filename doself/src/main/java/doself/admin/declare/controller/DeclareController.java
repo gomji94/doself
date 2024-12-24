6fb4cf4d3a1add3cbf90ada65e770ffd8cbe761d
gomji94 <mjh6958@gmail.com>
2024-12-24 16:27:44 +0900

    [doself-mjh] 수정 주제 detail : 수정 세부사항

---
+++ doself/src/main/java/doself/admin/declare/controller/DeclareController.java
@@ -0,0 +1,30 @@
+package doself.admin.declare.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+@RequestMapping("/admin/declare")
+public class DeclareController {
+
+	@GetMapping("list")
+	public String declareList(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("currentURI", request.getRequestURI());
+		model.addAttribute("title", "신고접수");
+		return "admin/declare/admin-declare-list";
+	}
+	
+	@GetMapping("userlist")
+	public String userHistoryList(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("currentURI", request.getRequestURI());
+		model.addAttribute("title", "부정회원관리");
+		return "admin/declare/user-list";
+	}
+	
+}
--- doself/src/main/java/doself/admin/member/controller/MemberController.java
+++ doself/src/main/java/doself/admin/member/controller/MemberController.java
@@ -5,14 +5,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
+
 @Controller
 @RequestMapping("/admin/member")
 public class MemberController {
 	
-	@GetMapping("/memberList")
-	public String memberListView(Model model) {
+	// 회원관리
+	@GetMapping("/list")
+	public String memberListView(HttpServletRequest request, Model model) {
 		
+		model.addAttribute("currentURI", request.getRequestURI());
 		model.addAttribute("title", "회원목록");
-		return "admin/member/memberList";
+		return "admin/index";
+	}
+	// 로그관리
+	@GetMapping("/loglist")
+	public String memberLog(HttpServletRequest request, Model model) {
+		
+		model.addAttribute("currentURI", request.getRequestURI());
+		model.addAttribute("title", "회원로그목록");
+		return "admin/member/log-list";
 	}
 }
