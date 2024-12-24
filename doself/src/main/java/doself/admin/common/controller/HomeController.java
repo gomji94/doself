bf5ee3d8769374d24e4baea5b3e7f3c5b342e8e6
gomji94 <mjh6958@gmail.com>
2024-12-24 10:27:28 +0900

    [doself-mjh] 수정 주제 detail : 수정 세부사항

--- doself/src/main/java/doself/admin/common/controller/HomeController.java
+++ doself/src/main/java/doself/admin/common/controller/HomeController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/admin")
 public class HomeController {
 	
-	@GetMapping(value={"","/"})
+	@GetMapping("/")
 	public String adminHome() {
 		
 		return "admin/index";
