93c96c82d996109464ecd294a4b4de157898e076
gomji94 <mjh6958@gmail.com>
2024-12-31 16:32:30 +0900

    [doself-mjh] db연동 테스트 detail : db연동 테스트

---
+++ doself/src/main/java/doself/admin/challenge/mapper/ChallengeMapper.java
@@ -0,0 +1,8 @@
+package doself.admin.challenge.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface ChallengeMapper {
+
+}
--- doself/src/main/java/doself/admin/member/controller/MemberController.java
+++ doself/src/main/java/doself/admin/member/controller/MemberController.java
@@ -1,22 +1,34 @@
 package doself.admin.member.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import doself.admin.member.domain.MemberDTO;
+import doself.admin.member.service.MemberService;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
 
 @Controller
 @RequestMapping("/admin/member")
+@RequiredArgsConstructor
 public class MemberController {
+	
+	private final MemberService memberService;
 	
 	// 회원관리 조회
 	@GetMapping("/list")
 	public String getMemberList(HttpServletRequest request, Model model) {
 		
+		List<MemberDTO> memberList = memberService.getMemberList();
+		
 		model.addAttribute("currentURI", request.getRequestURI());
 		model.addAttribute("title", "회원목록");
+		model.addAttribute("memberList", memberList);
+		
 		return "admin/index";
 	}
 	
@@ -32,9 +44,10 @@ public class MemberController {
 	// 로그관리 조회
 	@GetMapping("/loglist")
 	public String getMemberLog(HttpServletRequest request, Model model) {
-		
+
 		model.addAttribute("currentURI", request.getRequestURI());
 		model.addAttribute("title", "회원로그목록");
+		
 		return "admin/member/log-list";
 	}
 }
