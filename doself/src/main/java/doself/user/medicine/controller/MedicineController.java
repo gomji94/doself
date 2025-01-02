68503b746f735461fc5c3c17eb9bd0348765ff6f
sn1106 <esn157@gmail.com>
2025-01-02 11:52:04 +0900

    [doself-sn] Medicine 백엔드 작업 detail : Medicine 리스트 조회 및 상세 내역 조회 구현

--- doself/src/main/java/doself/user/medicine/controller/MedicineController.java
+++ doself/src/main/java/doself/user/medicine/controller/MedicineController.java
@@ -1,20 +1,36 @@
 package doself.user.medicine.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import doself.user.medicine.service.MedicineService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
 @RequestMapping("/medicine")
+@RequiredArgsConstructor
+@Slf4j
 public class MedicineController {
 	
+	private final MedicineService medicineService;
+	
 	@GetMapping("/list")
-	public String getMedicineList() {
+	public String getMedicineList(Model model) {
+		
+		model.addAttribute("medicineList", medicineService.getMedicineList());
+		
 		return "user/medicine/list";
 	}
 	
 	@GetMapping("/view")
-	public String getMedicineDetail() {
+	public String getMedicineDetail(@RequestParam(name = "medicineKeyNum") String medicineKeyNum, Model model) {
+		
+		model.addAttribute("medicineInfo", medicineService.getMedicineDetail(medicineKeyNum));
+		
 		return "user/medicine/view";
 	}
 	
