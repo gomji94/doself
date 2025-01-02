3acce8a10dd211fdcccda2c000049cfefb794759
sn1106 <esn157@gmail.com>
2025-01-02 11:10:33 +0900

    [doself-sn] Food 백엔드 작업 detail : Food 목록 조회 및 상세 조회 구현

--- doself/src/main/java/doself/user/food/controller/FoodController.java
+++ doself/src/main/java/doself/user/food/controller/FoodController.java
@@ -1,20 +1,44 @@
 package doself.user.food.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import doself.user.food.domain.Food;
+import doself.user.food.service.FoodService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("food")
+@Slf4j
 public class FoodController {
+	
+	private final FoodService foodService;
 
 	@GetMapping("/list")
-	public String getFoodList() {
+	public String getFoodList(Model model) {
+		List<Food> foodList = foodService.getFoodList();
+		
+		// log.info("foodList : {}", foodList);
+		
+		model.addAttribute("foodList", foodList);
+		
 		return "user/food/list";
 	}
 	
 	@GetMapping("/view")
-	public String getFoodDetail() {
+	public String getFoodDetail(@RequestParam(name = "foodKeyNum") String foodKeyNum, Model model) {
+		
+		// log.info("food : {}", foodService.getFoodDetail(foodKeyNum));
+		
+		model.addAttribute("foodInfo", foodService.getFoodDetail(foodKeyNum));
+		
 		return "user/food/view";
 	}
 	
---
+++ doself/src/main/java/doself/user/food/service/FoodServiceImpl.java
@@ -0,0 +1,33 @@
+package doself.user.food.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import doself.user.food.domain.Food;
+import doself.user.food.mapper.FoodMapper;
+import lombok.RequiredArgsConstructor;
+
+@Transactional
+@Service
+@RequiredArgsConstructor
+public class FoodServiceImpl implements FoodService {
+	
+	private final FoodMapper foodMapper;
+	
+	@Override
+	public List<Food> getFoodList() {
+		// TODO Auto-generated method stub
+		return foodMapper.getFoodList();
+	}
+
+	@Override
+	public Food getFoodDetail(String foodKeyNum) {
+		// TODO Auto-generated method stub
+		return foodMapper.getFoodDetail(foodKeyNum);
+	}
+	
+	
+
+}
