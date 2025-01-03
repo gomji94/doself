4d6e5f40585b8e97a88b3c9305dab06a525da922
sn1106 <esn157@gmail.com>
2025-01-03 14:58:34 +0900

    [doself-sn] Community 백엔드 구현 detail : Community 게시글 리스트 조회 및 카테고리별 조회 구현

--- doself/src/main/java/doself/user/community/controller/CommunityController.java
+++ doself/src/main/java/doself/user/community/controller/CommunityController.java
@@ -1,16 +1,50 @@
 package doself.user.community.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import doself.user.community.service.CommunityService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 @Controller
 @RequestMapping("/community")
+@RequiredArgsConstructor
+@Slf4j
 public class CommunityController {
 	
+	private final CommunityService communityService;
+	
 	@GetMapping("/list")
-	public String getCommunityList() {
+	public String getCommunityList(Model model) {
+		model.addAttribute("categoryList", communityService.getArticleCategoryList());
+		model.addAttribute("articleList", communityService.getArticleList());
+		
 		return "user/community/community-list";
 	}
+	
+	@GetMapping("/list/category")
+	public String getArticleListByCategory(@RequestParam(name = "cateNum") String categoryCode, Model model) {
+		
+		model.addAttribute("categoryList", communityService.getArticleCategoryList());
+		model.addAttribute("articleList", communityService.getArticleListByCategory(categoryCode));
+		
+		return "user/community/community-list";
+	}
+	
+	@GetMapping("/view")
+	public String getArticleDetail(@RequestParam(name = "articleNum") String articleKeyNum) {
+		
+		
+		return new String();
+	}
+	
+	
+	
 }
