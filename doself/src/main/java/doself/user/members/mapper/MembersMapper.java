b68f7a66836d8592705fa461e85fae12cf6fd888
ims1228802 <ims1228802@gmail.com>
2025-01-03 17:58:46 +0900

    [doself-shw] 회원 수정,검증 추가 detail : 회원 수정,검증 추가

--- doself/src/main/java/doself/user/members/mapper/MembersMapper.java
+++ doself/src/main/java/doself/user/members/mapper/MembersMapper.java
@@ -10,12 +10,14 @@ public interface MembersMapper {
 	//회원 정보 조회
 	public Members getMemberInfoById(String memberId);
 	
-	// 특정회원 수정
-	int modifyMember(Members member);
+	//회원 수정
+	public int modifyMember(Members member);
+	
+	//회원 비밀번호 검증
+	public int passwordChk(Members member);
 	
 	// 특정회원 탈퇴
-	int removeLoginHistoryById(String memberId);
-	
+	public int removeLoginHistoryById(String memberId);
 	
 	
 }
--- doself/src/main/java/doself/user/members/service/MembersService.java
+++ doself/src/main/java/doself/user/members/service/MembersService.java
@@ -7,4 +7,9 @@ public interface MembersService {
 	//회원 정보 조회
 	public Members getMemberInfoById(String memberId);
 	
+	//회원 정보 수정
+	public int modifyMember(Members member);
+
+	public int passwordChk(Members member);
+	
 }
