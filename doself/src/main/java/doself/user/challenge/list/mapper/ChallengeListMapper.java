8a9eec68aa93f21f5c8d10c80c98922a284f9980
korea <korea@DESKTOP-0T3SEIH>
2025-01-07 00:01:14 +0900

    [doself-kny] 챌린지 리스트 페이지 작업중 detail : 챌린지 리스트 페이지 작업중(페이지당 12개 제한)

--- doself/src/main/java/doself/user/challenge/list/mapper/ChallengeListMapper.java
+++ doself/src/main/java/doself/user/challenge/list/mapper/ChallengeListMapper.java
@@ -5,6 +5,9 @@ import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 import doself.user.challenge.list.domain.ChallengeList;
+import doself.util.CardPageable;
+import doself.util.PageInfo;
+import doself.util.Pageable;
 
 @Mapper
 public interface ChallengeListMapper {
@@ -14,5 +17,12 @@ public interface ChallengeListMapper {
 	// 특정 챌린지 정보 조회(detail view)
 	ChallengeList getChallengeListView(String ChallengeCode);
 
+	// 챌린지 추가(작업중)
 	int addChallenge(ChallengeList challengeList);
+	
+	// 챌린지 페이지
+	List<ChallengeList> getChallengePage(CardPageable cardPageable);
+	
+	// 챌린지 총 갯수
+	int getCntChallengeList();
 }
