315eb906f80ac14d936a5a72de6ff53ab36a768f
sn1106 <esn157@gmail.com>
2025-01-08 15:55:14 +0900

    [doself-sn] 마켓 구매 백엔드 구현 detail : 마켓 구매 백엔드 구현

--- doself/src/main/java/doself/user/market/controller/MarketController.java
+++ doself/src/main/java/doself/user/market/controller/MarketController.java
@@ -5,11 +5,14 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import doself.user.community.domain.Article;
+import doself.user.market.domain.MarketItem;
 import doself.user.market.domain.PurchaseItem;
+import doself.user.market.domain.PurchaseItemInfo;
 import doself.user.market.service.MarketService;
 import doself.util.Pageable;
 import jakarta.servlet.http.HttpSession;
@@ -34,20 +37,40 @@ public class MarketController {
 	}
 	
 	@GetMapping("/view")
-	public String getItemDetail(@RequestParam(name = "pointItemKeyNum") String pointItemKeyNum, Model model) {
+	public String getItemDetail(@RequestParam(name = "pointItemKeyNum") String pointItemKeyNum, Model model, HttpSession session) {
 		
-		model.addAttribute("itemInfo", marketService.getItemDetail(pointItemKeyNum));
+		String memberId = (String) session.getAttribute("SID");
+		MarketItem itemInfo = marketService.getItemDetail(pointItemKeyNum);
+		itemInfo.setMemberPoint(marketService.getMemberPointById(memberId));
+		model.addAttribute("itemInfo", itemInfo);
 		
 		return "user/market/view";
 	}
 	
 	@GetMapping("/purchase")
-	public String createPurchaseItem(@RequestParam(name = "pointItemKeyNum") String pointItemKeyNum, Model model) {
+	public String createPurchaseItem(@RequestParam(name = "pointItemKeyNum") String pointItemKeyNum, Model model, HttpSession session) {
 		
-		model.addAttribute("itemInfo", marketService.getItemDetail(pointItemKeyNum));
+		String memberId = (String) session.getAttribute("SID");
+		MarketItem itemInfo = marketService.getItemDetail(pointItemKeyNum);
+		itemInfo.setMemberPoint(marketService.getMemberPointById(memberId));
+		model.addAttribute("itemInfo", itemInfo);
 		
 		return "user/market/purchase-item";
 	}
+	
+	@PostMapping("/purchaseitem")
+	public String createPurchaseItem(PurchaseItemInfo purchaseItemInfo, HttpSession session) {
+		//TODO: process POST request
+		
+		String memberId = (String) session.getAttribute("SID");
+		purchaseItemInfo.setMemberId(memberId);
+		
+		System.out.println("================> " + purchaseItemInfo.toString());
+		
+		
+		return "user/market/purchase-list";
+	}
+	
 	
 	@GetMapping("/purchaselist")
 	public String getPurchaseListById(Pageable pageable, Model model, HttpSession session) {
---
+++ doself/src/main/java/doself/user/market/domain/PurchaseItemInfo.java
@@ -0,0 +1,12 @@
+package doself.user.market.domain;
+
+import lombok.Data;
+
+@Data
+public class PurchaseItemInfo {
+	
+	private String memberId;
+	private String pointItemKeyNum;
+	private int inputPointValue;
+	
+}
