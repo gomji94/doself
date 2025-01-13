e9a34dacb34846c85ff7290c098315ce80087d42
wlsgus4186 <wlsgus4186@daum.net>
2025-01-13 17:34:55 +0900

    [doself-jh] 영양 정보 분석표 개선 작업 및 css 조정 detail : 영양 정보 분석표 개선 작업 및 css 조정

--- doself/src/main/java/doself/user/feed/service/FeedService.java
+++ doself/src/main/java/doself/user/feed/service/FeedService.java
@@ -3,6 +3,7 @@ package doself.user.feed.service;
 import java.util.List;
 
 import doself.user.feed.domain.Feed;
+import doself.user.feed.mapper.FeedMapper;
 
 public interface FeedService {
 	// 피드 리스트 조회
@@ -10,4 +11,10 @@ public interface FeedService {
 	
 	// 특정 피드 상세 조회
 	Feed getFeedDetail(String feedCode);
+	
+	// 피드 추가
+	void addFeed(Feed feed);
+	
+	// 자동완성 검색
+	List<String> getKeywords(String query);
 }
