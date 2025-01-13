46aec05f646da9aa97391a86df355f44eb679b8a
ims1228802 <ims1228802@gmail.com>
2025-01-13 17:29:49 +0900

    [doself-shw] 티켓구매상세리스트 수정 detail : 티켓구매상세리스트 수정

--- doself/src/main/java/doself/user/ticket/service/TicketServiceImpl.java
+++ doself/src/main/java/doself/user/ticket/service/TicketServiceImpl.java
@@ -1,14 +1,18 @@
 package doself.user.ticket.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Service;
 
 import doself.user.ticket.domain.TicketItem;
+import doself.user.ticket.domain.TicketPurchase;
 import doself.user.ticket.mapper.TicketMapper;
+import doself.util.PageInfo;
+import doself.util.Pageable;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.log4j.Log4j;
 import lombok.extern.slf4j.Slf4j;
 
 @Service
@@ -19,9 +23,32 @@ public class TicketServiceImpl implements TicketService{
 
 	private final TicketMapper ticketMapper;
 	
+	// 티켓 아이템 조회
 	@Override
 	public List<TicketItem> getTicketList() {
 		return ticketMapper.getTicketList();
+	}
+	
+	// 티켓 상세내역 조회
+	@Override
+	public PageInfo<TicketPurchase> getTicketHistory(String memberId, Pageable pageable, String startDate, String endDate) {
+		int rowCnt = ticketMapper.getCntTicketHistory(memberId,startDate,endDate);
+		Map<String, Object> paramMap = new HashMap<String, Object>();
+		paramMap.put("memberId", memberId);
+		paramMap.put("rowPerPage", pageable.getRowPerPage());
+		paramMap.put("offset", pageable.getOffset());
+		paramMap.put("startDate", startDate);
+		paramMap.put("endDate", endDate);
+		List<TicketPurchase> ticketList = ticketMapper.getTicketListById(paramMap);
+		return new PageInfo<>(ticketList, pageable, rowCnt);
+	}
+
+	
+	
+	// 사용자 티켓정보
+	@Override
+	public TicketPurchase getPurchaseById(String memberId) {
+		return null;
 	}
 	
 }
